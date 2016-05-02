1ffa8cd310e10106e74f2ac751f47c1063e3262d
Max Häkkä <hakka@kth.se>
2016-05-02 14:32:38 +0200

    Added initial files

---
+++ app.js
@@ -0,0 +1,49 @@
+(function() {
+
+  var output = document.querySelector('#output'),
+    input = document.querySelector('#input'),
+    button = document.querySelector('#button'),
+    avatar = document.querySelector('#avatar'),
+    presence = document.querySelector('#presence');
+  var channel = 'mchat';
+
+  // Assign a random avatar in random color
+  avatar.className = 'face-' + ((Math.random() * 13 + 1) >>> 0) + ' color-' + ((Math.random() * 10 + 1) >>> 0);
+
+  var p = PUBNUB.init({
+    subscribe_key: 'sub-c-182105ac-0001-11e5-8fd4-0619f8945a4f',
+    publish_key: 'pub-c-ce04f67b-0f26-43ce-8be2-192e9821d1a3'
+  });
+
+  p.subscribe({
+    channel: channel,
+    callback: function(m) {
+      output.innerHTML = '<p><i class="' + m.avatar + '"></i><span>' + m.text.replace(/[<>]/ig, '') + '</span></p>' + output.innerHTML;
+    },
+    presence: function(m) {
+      if (m.occupancy > 1) {
+        presence.textContent = m.occupancy + ' people online';
+      } else {
+        presence.textContent = 'Nobody else is online';
+      }
+    }
+  });
+
+  p.bind('keyup', input, function(e) {
+    (e.keyCode || e.charCode) === 13 && publish()
+  });
+
+  p.bind('click', button, publish);
+
+  function publish() {
+    p.publish({
+      channel: channel,
+      message: {
+        avatar: avatar.className,
+        text: input.value
+      },
+      x: (input.value = '')
+    });
+  }
+
+})();
